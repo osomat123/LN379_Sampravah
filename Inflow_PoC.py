d4f91fe40197806cd191107430e4c901b9a8e5b7
arsh-code <arshkumar304@gmail.com>
2020-08-02 16:15:55 +0530

    Added Inflow for Model

---
+++ Inflow_PoC.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+
+def floodPredict(weir, timestamp):
+
+    enumerate(weir)
+    enumerate(timestamp)
+    height, area = 53, 81.25
+    h, q, time, prev = 0, 0, 0, 0
+    x, y, x2, n, xy = 0, 0, 0, 0, 0
+    m, c, d = 0, 0, 0
+    case = False
+    inflow = []
+    d0 = timestamp[0]
+    for i in range(0, 49):
+
+        h = (weir[i+1]-weir[i])*area
+        dur = timestamp[i+1]-timestamp[i]
+        sec = int(t.total_seconds())
+        q = h/sec
+        if(prev > q ^ case):
+            case = not case
+            d0 = timestamp[i-1]
+            x, y, x2, n, xy = 0, prev, 0, 1, 0
+        prev = q
+        n += 1
+        d1 = timestamp[i]
+        t = d1 - d0
+        time = int(t.total_seconds())
+        y += q
+        x += time
+        x2 += time*time
+        xy += time*q
+
+    d = n*x2 - x*x
+    c = (y*x2 - x*xy)/d
+    m = (n*xy - x*y)/d
+
+    return (inflow, m)
